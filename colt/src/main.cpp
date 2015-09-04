7e2be1ad3b2fe8e93007beed25c88fc7b8bd3da3
andrew-murray <andrew.2013.murray@gmail.com>
2015-09-04 23:16:54 +0100

    Project setup.
    - CMakeLists.txt
       - Include boost
       - Add main project executable
       - Add test project executable (using Boost test)
    - Other files
       - Stubs to handle boost_test and a main function for the project including boost::program_options

---
+++ colt/src/main.cpp
@@ -0,0 +1,6 @@
+#include <boost/program_options.hpp>
+
+int main(int argc, const char* argv[])
+{
+    return 0;
+}
---
+++ colt/test/test_help.cpp
@@ -0,0 +1,10 @@
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(help_test)
+
+BOOST_AUTO_TEST_CASE(null_test)
+{
+	BOOST_CHECK(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
