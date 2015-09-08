b4d99d8e8246fa8c5f1db592a019a605f3cf4748
andrew-murray <andrew.2013.murray@gmail.com>
2015-09-08 20:58:14 +0100

    Move boost usage to dlls, dealing with lots of weirdness in cmake's findboost.
    - CMakeLists.txt
       - Boost dynamic linking & dll copying
    - boost_test_module
       - Fix including the wrong boost/test header

--- colt/test/boost_test_module.cpp
+++ colt/test/boost_test_module.cpp
@@ -1,2 +1,2 @@
-#define BOOST_TEST_MODULE "command_line_template_tests"
-#include <boost/test/included/unit_test.hpp>
+#define BOOST_TEST_MODULE command_line_template_tests
+#include <boost/test/unit_test.hpp>
